bfe161d356563f0615fcb2e72d598bd0ff503e7f
Alexander Eberspächer <alex.eberspaecher@gmail.com>
2015-07-03 17:21:52 +0200

    Minor code cosmetics

--- isrepo.py
+++ isrepo.py
@@ -7,12 +7,10 @@ Print the status/revision of the current repository.
 Used for my custom prompt.
 """
 
-# git directory traversal and SVN code borrowed from S. Majewsky, tanks a lot!
+# SVN code and original git code borrowed from S. Majewsky, tanks a lot!
 #http://quickgit.kde.org/index.php?p=scratch%2Fmajewsky%2Fdevenv.git&a=blob&h=2405b0531653d9b833a785a135e68eaf658caebf&hb=841eb992e6c8adae5e831f6d43b8d5f373d15338&f=bin%2Fprettyprompt.py
 # Link taken from his blog post at
 # http://majewsky.wordpress.com/2011/09/13/a-clear-sign-of-madness/
-
-# TODO: improve documentation
 
 import os.path as op
 import sys
@@ -21,10 +19,11 @@ import subprocess as sp
 
 SHA1_length = 5  # print this many characters of SHA1 strings
 dirty_appendix = "*"
+at_character = "@"  # connect branch and commit with this character, e.g. master@e7123
 
 
 def cat_file(file_name):
-    """ Returns the content of the given file. No exception handling.
+    """ Returns the content of the given file. No real exception handling.
     """
 
     try:
@@ -34,10 +33,15 @@ def cat_file(file_name):
 
 
 class NotARepoException(Exception):
+    """Raised if path is not part of a repository.
+    """
+
     pass
 
 
-class CommandFailedException(Exception):
+class GitFailure(Exception):
+    """Raised if a command fails.
+    """
     pass
 
 
@@ -49,9 +53,12 @@ def recognize_git_repo(path):
     Throws NotARepoException if .git cannot be found.
     """
 
+    # TODO: alternative to directory traversal in repo recovery: check git exit status.
+    # 128 means something went wrong. to checkout: which other causes may lead to 128?
+
     base_path = op.realpath(path)
     sub_path = ""
-    # find Git repo (resides in a .git directory)
+    # find git repo (resides in a .git directory):
     while not op.exists(op.join(base_path, ".git")):
         # ascend in directory hierarchy if possible:
         base_path, new_sub_dir = op.split(base_path)
@@ -67,12 +74,12 @@ def recognize_git_repo(path):
 
     try:
         commit = get_git_commit(path)[:SHA1_length]
-    except CommandFailedException:  # TODO: can there be other exceptions raised?
+    except GitFailure:  # TODO: can there be other exceptions raised?
         commit = "before initial commit"
 
     is_dirty_appendix = get_git_dirty_string(path, dirty_appendix)
 
-    status = branch + "@" + commit + is_dirty_appendix
+    status = branch + at_character + commit + is_dirty_appendix
 
     return base_path, sub_path, status
 
@@ -87,7 +94,7 @@ def get_git_branch(path):
                                  stderr=sp.STDOUT)
     except sp.CalledProcessError:
         # interpret git returning an error exit code as not being on a branch:
-        raise CommandFailedException  # TODO: rename this exception to something git-ty
+        raise GitFailure
 
     return branch.replace("\n", "").split("/")[-1]
 
@@ -101,14 +108,14 @@ def get_git_commit(path):
                                  universal_newlines=True, cwd=path, shell=False,
                                  stderr=sp.STDOUT)
     except sp.CalledProcessError:
-        raise CommandFailedException
+        raise GitFailure
 
     return commit.replace("\n", "")
 
 
 def get_git_dirty_string(path, dirty_appendix="*"):
-    """Return '*' if the working directory is a 'dirty' git repository, ''
-    else.
+    """Return a specified character if the working directory is a 'dirty' git
+    repository, '' else.
     """
 
     try:
@@ -146,21 +153,22 @@ def recognize_svn_repo(path):
     for line in svn_info_output.splitlines():
         if line.startswith("Revision: "):
             revision = line[10:]
+    rev_string = "rev. {}".format(revision)
 
-    return base_path, sub_path, "rev. %s"%revision
+    return base_path, sub_path, rev_string
 
 
-if(__name__ == '__main__'):
+if __name__ == '__main__':
     try:  # is sys.argv[1] in a git repo?
         path, repopath, status = recognize_git_repo(sys.argv[1])
     except NotARepoException:
-        try:  # test for svn
+        try:  # test sys.argv[1] for being a svn repo
             path, repopath, status = recognize_svn_repo(sys.argv[1])
         except NotARepoException:
             status = ""
 
     # print prompt:
-    if(len(status) > 0):
+    if len(status) > 0:
         print("["+ status + "] ")
     else:
-        print("")
+        print(status)
